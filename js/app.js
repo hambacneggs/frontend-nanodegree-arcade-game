b65a17ee2fae86af88b057559ead9b9797ebdb17
Jon Osgood <jon360@gmail.com>
2016-08-30 14:14:01 -0700

    Remove instruction comments in app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,16 @@
 var allEnemies = [];
 
-// Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.col = 0;
     this.row = this.randomNum(1, 4);
     this.x = -101;
     this.y = (this.row * 83) - 30;
     this.speed = this.randomNum(100, 800);
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     allEnemies.push(this);
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     if (this.x < 606) {
         var timedSpeed = this.speed * dt;
         this.x = this.x + timedSpeed;
@@ -31,7 +21,6 @@ Enemy.prototype.update = function(dt) {
             player.reset();
         }
     } else {
-        // reset enemy position and speed
         this.row = this.randomNum(1, 4);
         this.y = (this.row * 83) - 30;
         this.x = -101;
@@ -39,7 +28,6 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -59,9 +47,6 @@ Enemy.prototype.setEnemyCol = function (x) {
     }
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Player = function() {
     this.reset();
     this.sprite = 'images/char-boy.png';
@@ -107,17 +92,13 @@ Player.prototype.reset = function () {
     playerPos = this.row + "" + this.col;
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 player = new Player();
 
 bob = new Enemy();
 frank = new Enemy();
 jim = new Enemy();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
